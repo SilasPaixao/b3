2189a69c254687e292f5ac12aae59d289fdf683e
Silas Paixão <silas.paixao873@gmail.com>
2024-04-23 01:39:03 -0300

    feat: ensure AddStockController returns 500 if AddStockUseCase throws

--- src/presentation/controllers/stocks/add-stock/add-stock-controller.ts
+++ src/presentation/controllers/stocks/add-stock/add-stock-controller.ts
@@ -1,24 +1,28 @@
 import { Controller, HttpRequest, HttpResponse, Validation, AddStock } from './add-stock-controller-protocols'
-import { badRequest } from '../../../helpers/http/http-helper'
+import { badRequest, serverError } from '../../../helpers/http/http-helper'
 
 export class AddStockController implements Controller {
-  constructor (
+  constructor(
     private readonly validation: Validation,
     private readonly addStock: AddStock
-  ) {}
+  ) { }
 
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return badRequest(error)
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+      const { year, stock, acronym, lucro } = httpRequest.body
+      await this.addStock.add({
+        year,
+        stock,
+        acronym,
+        lucro
+      })
+    } catch (error) {
+      return serverError(error)
     }
-    const {year, stock, acronym, lucro} = httpRequest.body
-    await this.addStock.add({
-      year,
-      stock,
-      acronym,
-      lucro
-    })
 
     return null
   }
