163c7192536bb742acfaa5a6f28d8a1ffd7f9408
Silas Paixão <silas.paixao873@gmail.com>
2024-04-22 23:58:31 -0300

    test: ensure AddStockController returns a badRequest error if validation fails

--- src/presentation/controllers/stocks/add-stock/add-stock-controller.spec.ts
+++ src/presentation/controllers/stocks/add-stock/add-stock-controller.spec.ts
@@ -1,6 +1,7 @@
 import { HttpRequest } from './add-stock-controller-protocols'
 import { AddStockController } from './add-stock-controller'
 import { Validation } from '../../../protocols'
+import { badRequest } from '../../../helpers/http/http-helper'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -11,18 +12,42 @@ const makeFakeRequest = (): HttpRequest => ({
   }
 })
 
-describe('AddStock Controller', () => {
-  test('Should call Validation with correct values', async () => {
-    class ValidationStub implements Validation {
-      validate (input: any): Error {
-        return null
-      }
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
     }
-    const validationStub = new ValidationStub()
+  }
+  return new ValidationStub()
+}
+
+interface SutTypes {
+  sut: AddStockController
+  validationStub: Validation
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation()
+  const sut = new AddStockController(validationStub)
+  return {
+    sut,
+    validationStub
+  }
+}
+
+describe('AddSurvey Controller', () => {
+  test('Should call Validation with correct values', async () => {
+    const { sut, validationStub } = makeSut()
     const validateSpy = jest.spyOn(validationStub, 'validate')
-    const sut = new AddStockController(validationStub)
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 400 if Validation fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new Error()))
   })
 })
