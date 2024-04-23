f1c9a2eeca8155eef5c7320879378dfd51444281
Silas Paixão <silas.paixao873@gmail.com>
2024-04-23 01:50:56 -0300

    test: ensure AddStockController returns 204 on success

--- src/presentation/controllers/stocks/add-stock/add-stock-controller.spec.ts
+++ src/presentation/controllers/stocks/add-stock/add-stock-controller.spec.ts
@@ -1,6 +1,6 @@
 import { HttpRequest, Validation, AddStock, AddStockModel } from './add-stock-controller-protocols'
 import { AddStockController } from './add-stock-controller'
-import { badRequest, serverError } from '../../../helpers/http/http-helper'
+import { badRequest, serverError, noContent } from '../../../helpers/http/http-helper'
 
 
 const makeFakeRequest = (): HttpRequest => ({
@@ -76,5 +76,11 @@ describe('AddStock Controller', () => {
     jest.spyOn(addStockStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 204 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(noContent())
   })
 })
