46304c488ac28cb02a4e4d56d049aaf2b26cade6
Silas Paixão <silas.paixao873@gmail.com>
2024-04-23 03:24:13 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/add-stock/db-add-stock.spec.ts
+++ src/data/usecases/add-stock/db-add-stock.spec.ts
@@ -8,16 +8,33 @@ const makeFakeStockData = (): AddStockModel => ({
   profit: '10%'
 })
 
+const makeAddStockRepository = (): AddStockRepository => {
+  class AddStockRepositoryStub implements AddStockRepository {
+    async add (stockData: AddStockModel): Promise<void> {
+      return new Promise(resolve => resolve())
+    }
+  }
+  return new AddStockRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbAddStock
+  addStockRepositoryStub: AddStockRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addStockRepositoryStub = makeAddStockRepository()
+  const sut = new DbAddStock(addStockRepositoryStub)
+  return {
+    sut,
+    addStockRepositoryStub
+  }
+}
+
 describe('DbAddStock Usecase', () => {
   test('Should call AddStockRepository with correct values', async () => {
-    class AddStockRepositoryStub implements AddStockRepository {
-      async add (stockData: AddStockModel): Promise<void> {
-        return new Promise(resolve => resolve())
-      }
-    }
-    const addStockRepositoryStub = new AddStockRepositoryStub()
+    const { sut, addStockRepositoryStub } = makeSut()
     const addSpy = jest.spyOn(addStockRepositoryStub, 'add')
-    const sut = new DbAddStock(addStockRepositoryStub)
     const stockData = makeFakeStockData()
     await sut.add(stockData)
     expect(addSpy).toHaveBeenCalledWith(stockData)
