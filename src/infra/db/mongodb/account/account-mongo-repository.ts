ab9c911b9ac45144f38d6f6084b82a6a1e229b46
Silas Paixão <silas.paixao873@gmail.com>
2024-05-06 13:03:59 -0300

    feat: ensure AccountMongoRepository find an account by token without role

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -30,4 +30,13 @@ async updateAccessToken(id: string, token: string): Promise<void> {
     { $set: { accessToken: token } }
   );
 }
+
+async loadByToken (token: string, role?: string): Promise<AccountModel> {
+  const accountCollection = await MongoHelper.getCollection('accounts')
+  const account = await accountCollection.findOne({
+    accessToken: token,
+    role
+  })
+  return account && MongoHelper.map(account)
+}
 }
