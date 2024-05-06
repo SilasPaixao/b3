838e851fc5b57163f46f3d2ad5dcef8802a961ac
Silas Paixão <silas.paixao873@gmail.com>
2024-05-06 14:23:03 -0300

    test: ensure add stock route returns 204 if valid access token is provided

--- src/main/routes/stock-routes.test.ts
+++ src/main/routes/stock-routes.test.ts
@@ -2,8 +2,11 @@ import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import { Collection } from 'mongodb'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
 
 let stockCollection: Collection
+let accountCollection: Collection
 
 describe('Stock Routes', () => {
   beforeAll(async () => {
@@ -17,6 +20,9 @@ describe('Stock Routes', () => {
   beforeEach(async () => {
     stockCollection = await MongoHelper.getCollection('stocks')
     await stockCollection.deleteMany({})
+
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /stocks', () => {
@@ -30,6 +36,33 @@ describe('Stock Routes', () => {
           profit: '10%'
         })
         .expect(403)
+    })
+    test('Should return 204 on add stock with valid accessToken', async () => {
+      const res = await accountCollection.insertOne({
+        name: 'Silas',
+        email: 'silas.paixao@gmail.com',
+        password: '123',
+        role: 'admin'
+      })
+      const id = res.insertedId
+      const accessToken = sign({ id }, env.jwtSecret)
+      await accountCollection.updateOne({
+        _id: id
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+      await request(app)
+        .post('/api/stocks')
+        .set('x-access-token', accessToken)
+        .send({
+          year: '2000',
+          stock: 'petrobras',
+          acronym: 'petr4',
+          profit: '10%'
+        })
+        .expect(204)
     })
   })
 })
