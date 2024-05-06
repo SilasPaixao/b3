0a43c3e6a0638cc4ec3361c0878d9e741b4b4385
Silas Paixão <silas.paixao873@gmail.com>
2024-05-06 13:27:48 -0300

    feat: ensure add survey route returns 204 if valid access token is provided

--- src/main/routes/stock-routes.ts
+++ src/main/routes/stock-routes.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddStockController } from '../factories/controllers/stock/add-stock/add-stock-controller-factory'
+import { adaptMiddleware } from '../adapters/express-middleware-adapter'
+import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
 
 export default (router: Router): void => {
-  router.post('/stocks', adaptRoute(makeAddStockController()))
+  const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
+  router.post('/stocks', adminAuth, adaptRoute(makeAddStockController()))
 }
