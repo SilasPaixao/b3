adbf7f4c7b9acf145aefeaa4f4e16b0a696eec9d
Silas Paixão <silas.paixao873@gmail.com>
2024-05-08 02:18:01 -0300

    feat: ensure LoadStocksController returns 500 if LoadStocksUseCase throws

--- src/presentation/controllers/stocks/load-stocks/load-stocks-controller.ts
+++ src/presentation/controllers/stocks/load-stocks/load-stocks-controller.ts
@@ -1,11 +1,15 @@
-import { ok } from '../../../helpers/http/http-helper'
+import { ok, serverError } from '../../../helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, LoadStocks } from './load-stocks-controller-protocols'
 
 export class LoadStocksController implements Controller {
-  constructor (private readonly loadStocks: LoadStocks) {}
+  constructor(private readonly loadStocks: LoadStocks) { }
 
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const stocks = await this.loadStocks.load()
-    return ok(stocks)
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const stocks = await this.loadStocks.load()
+      return ok(stocks)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
