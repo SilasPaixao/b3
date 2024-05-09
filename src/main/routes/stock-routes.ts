eb637c8ca056cf7a94c991a54a98511e21f212dc
Silas Paixão <silas.paixao873@gmail.com>
2024-05-09 02:17:20 -0300

    refactor: move auth middlewares to separate files

--- src/main/routes/stock-routes.ts
+++ src/main/routes/stock-routes.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddStockController } from '../factories/controllers/stock/add-stock/add-stock-controller-factory'
-import { adaptMiddleware } from '../adapters/express-middleware-adapter'
-import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
 import { makeLoadStocksController } from '../factories/controllers/stock/load-stocks/load-stocks-controller-factory'
+import { adminAuth } from '../middlewares/admin-auth'
+import { auth } from '../middlewares/auth'
 
 export default (router: Router): void => {
-  const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
-  const auth = adaptMiddleware(makeAuthMiddleware())
 
   router.post('/stocks', adminAuth, adaptRoute(makeAddStockController()))
   router.get('/stocks', auth, adaptRoute(makeLoadStocksController()))
