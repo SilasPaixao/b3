b3d1be5fe39f7060c28307a72a6de39d40aac11e
Silas Paixao <silas.paixao873@gmail.com>
2024-06-29 02:27:30 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,17 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'B3 - Ações campeãs',
+    description: 'B3 - Relatório das ações com maior retorno anual desde 2000',
+    version: '1.0.0',
+    contact: {
+      name: 'Silas Paixão',
+      email: 'silas.paixao873@gmail.com',
+      url: 'https://portfolio-silas.vercel.app/'
+    },
+    license: {
+      name: 'GPL-3.0-or-later',
+      url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
+    }
+  }
+}
