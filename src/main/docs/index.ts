7b37493272c6b989b9e82f7da35dbb3b7eb23c63
Silas Paixao <silas.paixao873@gmail.com>
2024-06-30 04:59:26 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,3 +1,9 @@
+import { notFound, serverError, unauthorized, badRequest} from './components'
+import { loginPath } from './paths/login-path'
+import { accountSchema } from './schemas/account-schema'
+import { errorSchema } from './schemas/error-schema'
+import { loginParamsSchema } from './schemas/login-params-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
@@ -12,6 +18,28 @@ export default {
     license: {
       name: 'GPL-3.0-or-later',
       url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
+    },
+    servers: [{
+      url: '/api',
+      description: 'Servidor Principal'
+    }],
+    tags: [{
+      name: 'Login',
+      description: 'APIs relacionadas a Login'
+    }],
+    paths: {
+      '/login': loginPath
+    },
+    schemas: {
+      account: accountSchema,
+      loginParams: loginParamsSchema,
+      error: errorSchema
+    },
+    components: {
+      badRequest: badRequest,
+      serverError: serverError,
+      unauthorized: unauthorized,
+      notFound: notFound
     }
   }
 }
